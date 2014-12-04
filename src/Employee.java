bd32019401a47808f8d10382a1e63ebfe1a5c755
Naimisha Badri <starzblue24@gmail.com>
2014-12-03 19:10:12 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,48 +1,57 @@
 // placeholder for your Employee class
-public class EmployeeWages {
-  public static void main(String[] args) {
-    // replace <put your name here> with your name!
-    System.out.println("Student: Naimisha Badri");
-    
-    // Create two instances of class Employee emp1 & emp2
-    Employee emp1= new Employee("Roger", 9.0, 40);
-    Employee emp2= new Employee("Susie", 11.0, 42);                             
-    
-    // print emp1 
-    System.out.println(emp1.toString()); 
-    
-    // print emp1's weekly salary (put a tab first) like "       weekly salary: $360.00" 
-    System.out.println("     -weekly salary: $"+emp1.getWeeklySalary());
-    
-    // print emp1's annual salary (put a tab first) like "       annual salary: $18,720.00" 
-    System.out.println("     -annual salary: $"+emp1.getYearlySalary());
-    
-    // print emp2
-    System.out.println(emp2.toString());
-    
-    // change emp2's name
-    emp2.setName("Susan");
-    
-    // change emp2's wage
-    emp2.setWage(11.5);
-    
-    // change emp2's hours
-    emp2.setHours(43);
-    
-    // print emp2 
-    System.out.println(emp2.toString());
-    
-    // print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
-    System.out.println("     -weekly salary: $"+emp2.getWeeklySalary());
-    
-    // print emp2's annual salary (put a tab first) like "       annual salary: $18,720.00" 
-    System.out.println("     -annual salary: $"+emp2.getYearlySalary());
-    
-    // give emp2 a raise
-    emp2.giveRaise();
-    
-    // print emp2
-    System.out.println(emp2.toString());
-    
+public class Employee
+{
+  String namePerson;
+  double hourlyWage;
+  int hoursWorked;
+  public Employee(String name, double wage, int hours)
+  {
+    namePerson=name;
+    hourlyWage=wage;
+    hoursWorked=hours;
+  }
+  public String getName()
+  {
+    return namePerson;
+  }
+  public double getWage()
+  {
+    return hourlyWage;
+  }
+  public int getHours()
+  {
+    return hoursWorked;
+  }
+  public double getWeeklySalary()
+  {
+    double x=hourlyWage*hoursWorked;
+    return x;
+  }
+  public double getYearlySalary()
+  {
+    double x=hourlyWage*hoursWorked;
+    x*=52;
+    return x;
+  }
+  public String toString()
+  {
+    String a="name: " + namePerson +", wage: $" + hourlyWage + ", hours: "+hoursWorked;
+    return a;
+  }
+  public void setName(String newName)
+  {
+    namePerson=newName;
+  }
+  public void setWage(double newWage)
+  {
+    hourlyWage=newWage;
+  }
+  public void setHours(int h)
+  {
+    hoursWorked=h;
+  }
+  public void giveRaise()
+  {
+    hourlyWage+=1;
   }
 }
