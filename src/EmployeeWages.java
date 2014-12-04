574dba15ce8fefc1e158c7745ecd878374c11bf4
Naimisha Badri <starzblue24@gmail.com>
2014-12-03 19:10:49 -0700

    Update EmployeeWages.java

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,34 +1,48 @@
 // placeholder for your main method
 public class EmployeeWages {
-    public static void main(String[] args) {
-        // replace <put your name here> with your name!
-        System.out.println("Student: <put your name here>");
-        
-        // Create two instances of class Employee emp1 & emp2
-        
-        // print emp1 
-        
-        // print emp1's weekly salary (put a tab first) like "       weekly salary: $360.00" 
-        
-        // print emp1's annual salary (put a tab first) like "       annual salary: $18,720.00" 
-        
-        // print emp2
-        
-        // change emp2's name
-        
-        // change emp2's wage
-        
-        // change emp2's hours
-        
-        // print emp2 
-        
-        // print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
-        
-        // print emp2's annual salary (put a tab first) like "       annual salary: $18,720.00" 
-        
-        // give emp2 a raise
-        
-        // print emp2
-        
-    }
+  public static void main(String[] args) {
+    // replace <put your name here> with your name!
+    System.out.println("Student: Naimisha Badri");
+    
+    // Create two instances of class Employee emp1 & emp2
+    Employee emp1= new Employee("Roger", 9.0, 40);
+    Employee emp2= new Employee("Susie", 11.0, 42);                             
+    
+    // print emp1 
+    System.out.println(emp1.toString()); 
+    
+    // print emp1's weekly salary (put a tab first) like "       weekly salary: $360.00" 
+    System.out.println("     -weekly salary: $"+emp1.getWeeklySalary());
+    
+    // print emp1's annual salary (put a tab first) like "       annual salary: $18,720.00" 
+    System.out.println("     -annual salary: $"+emp1.getYearlySalary());
+    
+    // print emp2
+    System.out.println(emp2.toString());
+    
+    // change emp2's name
+    emp2.setName("Susan");
+    
+    // change emp2's wage
+    emp2.setWage(11.5);
+    
+    // change emp2's hours
+    emp2.setHours(43);
+    
+    // print emp2 
+    System.out.println(emp2.toString());
+    
+    // print emp2's weekly salary (put a tab first) like "       weekly salary: $360.00" 
+    System.out.println("     -weekly salary: $"+emp2.getWeeklySalary());
+    
+    // print emp2's annual salary (put a tab first) like "       annual salary: $18,720.00" 
+    System.out.println("     -annual salary: $"+emp2.getYearlySalary());
+    
+    // give emp2 a raise
+    emp2.giveRaise();
+    
+    // print emp2
+    System.out.println(emp2.toString());
+    
+  }
 }
